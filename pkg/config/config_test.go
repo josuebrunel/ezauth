be71f03c089f0de6f5bd1ad53c8eeaf6ac7acb3b
Josue Kouka <josuebrunel@gmail.com>
2026-01-29 09:34:23 -0800

    fix ci/cd test errors (fix #35)

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -16,8 +16,10 @@ func TestLoadConfig_RequiredJWTSecret(t *testing.T) {
 }
 
 func TestLoadConfig_Success(t *testing.T) {
+	os.Setenv("EZAUTH_API_KEY", "test-api-key")
 	os.Setenv("EZAUTH_JWT_SECRET", "super-secret")
 	defer os.Unsetenv("EZAUTH_JWT_SECRET")
+	defer os.Unsetenv("EZAUTH_API_KEY")
 
 	cfg, err := LoadConfig()
 	if err != nil {
