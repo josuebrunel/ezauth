6f7e12bb80c6673749ba3c3e3a878ce02ac2b260
Josue Kouka <josuebrunel@gmail.com>
2026-02-11 10:03:51 -0800

    use goose provider to avoid global state (fix #63)

--- pkg/db/migrations/migrate.go
+++ pkg/db/migrations/migrate.go
@@ -1,6 +1,7 @@
 package migrations
 
 import (
+	"context"
 	"database/sql"
 	"embed"
 	"fmt"
@@ -23,23 +24,21 @@ const (
 //go:embed postgres sqlite mysql
 var embedMigrations embed.FS
 
-type migrationFunc func(db *sql.DB, dir string, opts ...goose.OptionsFunc) error
-
 func MigrateUp(dsn, dialect, schema string) error {
-	return runMigration(dsn, dialect, schema, goose.Up, "up")
+	return runMigration(dsn, dialect, schema, "up")
 }
 
 func MigrateDown(dsn, dialect, schema string) error {
-	return runMigration(dsn, dialect, schema, goose.Down, "down")
+	return runMigration(dsn, dialect, schema, "down")
 }
 
 func MigrateUpWithDBConn(db *sql.DB, dialect string) error {
 	// For existing DB connection, we assume schema setup is already done or not needed
-	return execGooseMigration(db, dialect, goose.Up, "up")
+	return execGooseMigration(db, dialect, "up")
 }
 
 func MigrateDownWithDBConn(db *sql.DB, dialect string) error {
-	return execGooseMigration(db, dialect, goose.Down, "down")
+	return execGooseMigration(db, dialect, "down")
 }
 
 func getMigrationSubDir(dialect string) (string, error) {
@@ -65,7 +64,7 @@ func getRootFS(subDir string) (fs.FS, error) {
 	return rootFS, nil
 }
 
-func runMigration(dsn, dialect, schema string, command migrationFunc, action string) error {
+func runMigration(dsn, dialect, schema string, action string) error {
 	if dsn == "" {
 		return fmt.Errorf("dsn is required")
 	}
@@ -88,10 +87,10 @@ func runMigration(dsn, dialect, schema string, command migrationFunc, action str
 		}
 	}
 
-	return execGooseMigration(db, dialect, command, action)
+	return execGooseMigration(db, dialect, action)
 }
 
-func execGooseMigration(db *sql.DB, dialect string, command migrationFunc, action string) error {
+func execGooseMigration(db *sql.DB, dialect string, action string) error {
 	migrationSubDir, err := getMigrationSubDir(dialect)
 	if err != nil {
 		xlog.Error("Failed to get migration dir")
@@ -103,21 +102,45 @@ func execGooseMigration(db *sql.DB, dialect string, command migrationFunc, actio
 		return err
 	}
 
-	if err := goose.SetDialect(dialect); err != nil {
-		return fmt.Errorf("failed to set dialect: %w", err)
+	var d goose.Dialect
+
+	switch dialect {
+	case DialectPostgres:
+		d = goose.DialectPostgres
+	case DialectMysql:
+		d = goose.DialectMySQL
+	case DialectSqlite, "sqlite3":
+		d = goose.DialectSQLite3
+	default:
+		return fmt.Errorf("unknown dialect: %s", dialect)
 	}
 
-	goose.SetTableName("ezauth_goose_db_version")
-	goose.SetBaseFS(rootFS)
+	provider, err := goose.NewProvider(d, db, rootFS, goose.WithTableName("ezauth_goose_db_version"))
+	if err != nil {
+		return fmt.Errorf("failed to create goose provider: %w", err)
+	}
 
 	xlog.Info(fmt.Sprintf("running migrations %s", action), "dialect", dialect, "folder", migrationSubDir)
-	if err := command(db, "."); err != nil {
+
+	var results []*goose.MigrationResult
+	if action == "up" {
+		results, err = provider.Up(context.Background())
+	} else if action == "down" {
+		var res *goose.MigrationResult
+		res, err = provider.Down(context.Background())
+		if res != nil {
+			results = append(results, res)
+		}
+	} else {
+		return fmt.Errorf("unknown action: %s", action)
+	}
+
+	if err != nil {
 		return fmt.Errorf("failed to run migrations %s: %w", action, err)
 	}
 
-	xlog.Info(fmt.Sprintf("migrations %s ran successfully", action))
+	xlog.Info(fmt.Sprintf("migrations %s ran successfully", action), "count", len(results))
 	return nil
-
 }
 
 func getDBConnection(dialect string, dsn string) (*sql.DB, error) {
